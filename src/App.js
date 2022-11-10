54a46a77ef01e26c1e4acc81bbb1dd24877cf38b
Darlington Bvudzijena <darlingtonbvudzijena@gmail.com>
2022-11-10 15:27:45 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Nav from "/components/Nav.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
